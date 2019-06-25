7a240a5fa19b8989faca2b7f1ba28c16845bf7d6
Maximilian Schmitt <maximilian.schmitt@googlemail.com>
2019-06-25 10:55:09 +0200

    Update tooling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-'use strict'
+"use strict";
 
-const React = require('react')
-const ReactDOM = require('react-dom')
-const scrollparent = require('scrollparent')
+const React = require("react");
+const ReactDOM = require("react-dom");
+const scrollparent = require("scrollparent");
 
 class Popover extends React.Component {
   static propTypes = {
@@ -14,235 +14,248 @@ class Popover extends React.Component {
     constrainX: React.PropTypes.bool,
     constrainY: React.PropTypes.bool,
     position: React.PropTypes.object
+  };
+
+  constructor() {
+    super();
+    this.onResize = this.onResize.bind(this);
   }
 
-  constructor () {
-    super()
-    this.onResize = this.onResize.bind(this)
-  }
+  componentDidMount() {
+    const domNode = ReactDOM.findDOMNode(this);
+    this.scrollParent = scrollparent(domNode);
 
-  componentDidMount () {
-    const domNode = ReactDOM.findDOMNode(this)
-    this.scrollParent = scrollparent(domNode)
-
-    this.popover = document.createElement('div')
-    this.popover.className = '_Popover'
-    this._detachedContainer = document.createElement('div')
-    this._detachedContainer.style.display = 'inline-block'
-    this._detachedContainer.style.visibility = 'hidden'
-    this._detachedContainer.style.position = 'absolute'
-    this._detachedContainer.style.top = 0
-    this._detachedContainer.style.left = 0
-    this._detachedContainer.className = this.props.className || 'Popover'
-    document.body.appendChild(this.popover)
-    document.body.appendChild(this._detachedContainer)
-    this._renderPopover()
+    this.popover = document.createElement("div");
+    this.popover.className = "_Popover";
+    this._detachedContainer = document.createElement("div");
+    this._detachedContainer.style.display = "inline-block";
+    this._detachedContainer.style.visibility = "hidden";
+    this._detachedContainer.style.position = "absolute";
+    this._detachedContainer.style.top = 0;
+    this._detachedContainer.style.left = 0;
+    this._detachedContainer.className = this.props.className || "Popover";
+    document.body.appendChild(this.popover);
+    document.body.appendChild(this._detachedContainer);
+    this._renderPopover();
 
     if (this.props.open) {
-      window.addEventListener('resize', this.onResize)
+      window.addEventListener("resize", this.onResize);
       if (this.scrollParent) {
-        this.scrollParent.addEventListener('scroll', this.onResize)
+        this.scrollParent.addEventListener("scroll", this.onResize);
       }
     }
   }
 
-  componentWillReceiveProps (props) {
+  componentWillReceiveProps(props) {
     // if we are now opening the popover
     if (!this.props.open && props.open) {
-      window.addEventListener('resize', this.onResize)
+      window.addEventListener("resize", this.onResize);
       if (this.scrollParent) {
-        this.scrollParent.addEventListener('scroll', this.onResize)
+        this.scrollParent.addEventListener("scroll", this.onResize);
       }
     }
 
     // if we are now hiding the popover
     if (this.props.open && !props.open) {
-      window.removeEventListener('resize', this.onResize)
+      window.removeEventListener("resize", this.onResize);
       if (this.scrollParent) {
-        this.scrollParent.removeEventListener('scroll', this.onResize)
+        this.scrollParent.removeEventListener("scroll", this.onResize);
       }
     }
   }
 
-  componentDidUpdate () {
-    this._renderPopover()
+  componentDidUpdate() {
+    this._renderPopover();
   }
 
-  componentWillUnmount () {
-    window.removeEventListener('resize', this.onResize)
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.onResize);
     if (this.scrollParent) {
-      this.scrollParent.removeEventListener('scroll', this.onResize)
+      this.scrollParent.removeEventListener("scroll", this.onResize);
     }
-    if (this.popover.parentNode !== document.body) return
-    ReactDOM.unmountComponentAtNode(this.popover)
-    document.body.removeChild(this.popover)
-    ReactDOM.unmountComponentAtNode(this._detachedContainer)
-    document.body.removeChild(this._detachedContainer)
+    if (this.popover.parentNode !== document.body) return;
+    ReactDOM.unmountComponentAtNode(this.popover);
+    document.body.removeChild(this.popover);
+    ReactDOM.unmountComponentAtNode(this._detachedContainer);
+    document.body.removeChild(this._detachedContainer);
   }
 
-  onResize () {
-    this.forceUpdate()
+  onResize() {
+    this.forceUpdate();
   }
 
-  getViewportBounds () {
-    const { x, y } = scrollPosition()
+  getViewportBounds() {
+    const { x, y } = scrollPosition();
+
     return {
       left: x,
       right: x + window.innerWidth,
       top: y,
       bottom: y + window.innerHeight
-    }
+    };
   }
 
-  getAnchorBounds () {
-    return this.getBounds(this.anchor)
+  getAnchorBounds() {
+    return this.getBounds(this.anchor);
   }
 
-  getBounds (domNode) {
-    const bounds = domNode.getBoundingClientRect()
-    const viewport = this.getViewportBounds()
+  getBounds(domNode) {
+    const bounds = domNode.getBoundingClientRect();
+    const viewport = this.getViewportBounds();
 
     const a = {
       left: bounds.left + viewport.left,
       right: bounds.right + viewport.left,
       top: bounds.top + viewport.top,
       bottom: bounds.bottom + viewport.top
-    }
+    };
 
-    return a
+    return a;
   }
 
-  getBodySize () {
+  getBodySize() {
     return {
       width: this._detachedContainer.offsetWidth,
       height: this._detachedContainer.offsetHeight
-    }
+    };
   }
 
-  calculatePosition () {
-    const docbounds = this.getViewportBounds()
-    const rect = this.getAnchorBounds()
-    const body = this.getBodySize()
+  calculatePosition() {
+    const docbounds = this.getViewportBounds();
+    const rect = this.getAnchorBounds();
+    const body = this.getBodySize();
 
-    const position = Object.assign({
-      x: calculateXPosition(),
-      y: calculateYPosition()
-    }, this.props.position || {})
+    const position = Object.assign(
+      {
+        x: calculateXPosition(),
+        y: calculateYPosition()
+      },
+      this.props.position || {}
+    );
 
-    const values = this.calculatePositionValues(position)
+    const values = this.calculatePositionValues(position);
 
     if (values.left < 0) {
-      position.x = 'left'
+      position.x = "left";
     }
 
-    return position
+    return position;
 
-    function calculateXPosition () {
+    function calculateXPosition() {
       if (rect.right + body.width >= docbounds.right) {
-        return 'right'
+        return "right";
       }
 
-      return 'left'
+      return "left";
     }
 
-    function calculateYPosition () {
+    function calculateYPosition() {
       if (rect.bottom + body.height >= docbounds.bottom) {
-        return 'top'
+        return "top";
       }
 
-      return 'bottom'
+      return "bottom";
     }
   }
 
-  calculatePositionValues (position) {
-    const { constrainTo, constrainX, constrainY } = this.props
-    const docbounds = this.getViewportBounds()
-    const bounds = this.getAnchorBounds()
-    const body = this.getBodySize()
-    const values = {}
+  calculatePositionValues(position) {
+    const { constrainTo, constrainX, constrainY } = this.props;
+    const docbounds = this.getViewportBounds();
+    const bounds = this.getAnchorBounds();
+    const body = this.getBodySize();
+    const values = {};
 
-    if (position.y === 'bottom') {
-      values.top = bounds.bottom
-    } else if (position.y === 'center') {
-      const anchorHeight = bounds.bottom - bounds.top
-      const anchorCenter = bounds.top + anchorHeight / 2
-      values.top = anchorCenter - body.height / 2
+    if (position.y === "bottom") {
+      values.top = bounds.bottom;
+    } else if (position.y === "center") {
+      const anchorHeight = bounds.bottom - bounds.top;
+      const anchorCenter = bounds.top + anchorHeight / 2;
+      values.top = anchorCenter - body.height / 2;
     } else {
-      values.top = bounds.top - body.height
+      values.top = bounds.top - body.height;
     }
 
-    if (position.x === 'left') {
-      values.left = bounds.left
-    } else if (position.x === 'center') {
-      const anchorWidth = bounds.right - bounds.left
-      const anchorCenter = bounds.left + anchorWidth / 2
-      values.left = anchorCenter - body.width / 2
+    if (position.x === "left") {
+      values.left = bounds.left;
+    } else if (position.x === "center") {
+      const anchorWidth = bounds.right - bounds.left;
+      const anchorCenter = bounds.left + anchorWidth / 2;
+      values.left = anchorCenter - body.width / 2;
     } else {
-      values.left = bounds.right - body.width
+      values.left = bounds.right - body.width;
     }
 
     if (values.left + body.width > docbounds.right) {
-      values.left = docbounds.right - body.width
+      values.left = docbounds.right - body.width;
     }
 
-    if (constrainTo === 'scrollParent') {
-      const scrollBounds = this.getBounds(this.scrollParent)
+    if (constrainTo === "scrollParent") {
+      const scrollBounds = this.getBounds(this.scrollParent);
       if (constrainX && values.left < scrollBounds.left) {
-        values.left = scrollBounds.left
+        values.left = scrollBounds.left;
       } else if (constrainX && values.left + body.width > scrollBounds.right) {
-        values.left = scrollBounds.right - body.width
+        values.left = scrollBounds.right - body.width;
       }
 
       if (constrainY && values.top < scrollBounds.top) {
-        values.top = scrollBounds.top
+        values.top = scrollBounds.top;
       } else if (constrainY && values.top + body.height > scrollBounds.bottom) {
-        values.top = scrollBounds.bottom - body.height
+        values.top = scrollBounds.bottom - body.height;
       }
     }
 
-    return values
+    return values;
   }
 
-  calculateStyle (position, values) {
-    const style = { position: 'absolute', zIndex: '201' }
-    return Object.assign(style, values || this.calculatePositionValues(position))
+  calculateStyle(position, values) {
+    const style = { position: "absolute", zIndex: "201" };
+
+    return Object.assign(
+      style,
+      values || this.calculatePositionValues(position)
+    );
   }
 
-  _renderPopover () {
-    this._renderDetached()
-    this.anchor = ReactDOM.findDOMNode(this)
-    const position = this.calculatePosition(this.anchor)
-    const style = this.calculateStyle(position)
-    const baseClassName = this.props.className || 'Popover'
+  _renderPopover() {
+    this._renderDetached();
+    this.anchor = ReactDOM.findDOMNode(this);
+    const position = this.calculatePosition(this.anchor);
+    const style = this.calculateStyle(position);
+    const baseClassName = this.props.className || "Popover";
 
-    const className = baseClassName +
-      (' ' + baseClassName + '--position-' + position.y + '-' + position.x)
+    const className = `${baseClassName} ${baseClassName}--position-${position.y}-${position.x}`;
 
     if (!this.props.open) {
-      ReactDOM.render((
-        <div style={{ display: 'none' }} className={className} />
-      ), this.popover)
-      return
+      ReactDOM.render(
+        <div style={{ display: "none" }} className={className} />,
+        this.popover
+      );
+
+      return;
     }
 
-    ReactDOM.render((
-      <div style={style} className={className}>{this.props.children}</div>
-    ), this.popover)
+    ReactDOM.render(
+      <div style={style} className={className}>
+        {this.props.children}
+      </div>,
+      this.popover
+    );
   }
 
-  _renderDetached () {
-    ReactDOM.render(this.props.children, this._detachedContainer)
+  _renderDetached() {
+    ReactDOM.render(this.props.children, this._detachedContainer);
   }
 
-  render () {
-    return this.props.around
+  render() {
+    return this.props.around;
   }
 }
 
-function scrollPosition () {
-  const x = window.pageXOffset || document.documentElement.scrollLeft
-  const y = window.pageYOffset || document.documentElement.scrollTop
-  return { x, y }
+function scrollPosition() {
+  const x = window.pageXOffset || document.documentElement.scrollLeft;
+  const y = window.pageYOffset || document.documentElement.scrollTop;
+
+  return { x, y };
 }
 
-module.exports = Popover
+module.exports = Popover;
